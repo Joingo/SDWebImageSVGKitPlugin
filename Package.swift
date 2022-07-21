14261479c1d166d11ebbde6304e84c77e901e46e
Brian Guertin <brian@joingo.com>
2022-07-21 17:01:05 -0500

    Update SVGKit

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0"),
-        .package(url: "https://github.com/Joingo/SVGKit.git", from: "3.0.1")
+        .package(url: "https://github.com/Joingo/SVGKit.git", from: "3.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
